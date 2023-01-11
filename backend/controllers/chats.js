a35b42be17c9f01062d7a225c2397fc3de94bbf5
pgold1738 <pgoldbeck1103@gmail.com>
2023-01-11 12:57:55 -0800

    users and chats controllers created and updated config.js file

---
+++ backend/controllers/chats.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+const jwt = require('jwt-simple')
+const config = require('../config/config')
+
+function isAuthenticated(req, res, next){
+    if(req.headers.authorization){
+        next()
+    } else {
+        res.sendStatus(401)
+    }
+}
+
+// create route
+router.post('/', isAuthenticated, async (req, res) => {
+    const newChat = await db.Chat.create(req.body)
+    const token = req.headers.authorization
+    const decoded = jwt.decode(token, config.jwtSecret)
+    newChat.user = decoded.id
+    newChat.save()
+    res.json(newChat)
+})
+
+// index route
+router.get('/', async (req, res) => {
+    const allChats = await db.Chat.find({}).populate('user')
+    res.json(allChats)
+})
+
+// show route
+router.get('/:id', async (req, res) => {
+    const foundChat = await db.Chat.findById(req.params.id).populate('user')
+    res.json(foundChat)
+})
+
+//update
+router.put('/:id', isAuthenticated, async (req, res) => {
+    const foundChat = await db.Chat.findById(req.params.id)
+    const token = req.headers.authorization
+    const decoded = jwt.decode(token, config.jwtSecret)
+    if(foundChat.user == decoded.id){
+        const updatedChat = await db.Chat.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        )
+        res.json(updatedChat)
+    }
+    const newChat = await db.Chat.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    )
+    res.json(newChat)
+})
+
+//delete
+router.delete('/:id', isAuthenticated, async (req, res)=> {
+    await db.Chat.findByIdAndDelete(req.params.id)
+    res.sendStatus(200)
+})
+
+module.exports = router
---
+++ backend/controllers/users.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+const jwt = require('jwt-simple')
+const config = require('../config/config')
+
+
+function isAuthenticated(req, res, next){
+    
+    if(req.headers.authorization){
+        next()
+    } else {
+        res.sendStatus(401)
+    }
+
+}
+
+
+//signup create route
+router.post('/signup', async (req, res) => {
+    // console.log(req.body)
+    const foundUser = await db.User.findOne({ username: req.body.username})
+    // console.log(foundUser)
+    if(!foundUser){
+        const createdUser = await db.User.create(req.body)
+        const payload = {id: createdUser._id}
+        const token = jwt.encode(payload, config.jwtSecret)
+        res.json({
+            user: createdUser,
+            token: token
+        })
+    } else {
+        res.sendStatus(401)
+    }
+})
+
+//login route
+router.post('/login', async (req, res) => {
+    const foundUser = await db.User.findOne({ username: req.body.username})
+    // console.log(foundUser)
+    if(req.body.password === foundUser.password){
+        const payload = {id: foundUser._id}
+        const token = jwt.encode(payload, config.jwtSecret)
+        const userChat = await db.Chat.find({ user: foundUser._id })
+        res.json({
+            user: foundUser,
+            token: token,
+            chat: userChat
+        })
+    } else {
+        res.sendStatus(401)
+    }
+})
+
+// token show
+router.get('/token', isAuthenticated, async (req, res) => {
+    const token = req.headers.authorization
+    const decoded = jwt.decode(token, config.jwtSecret)
+    const foundUser = await db.User.findById(decoded.id)
+    const userChat = await db.Chat.find({ user: foundUser._id })
+    res.json({
+        user: foundUser,
+        chat: userChat
+    })
+})
+
+// index 
+router.get('/', async (req, res) => {
+    const allUsers = await db.User.find({})
+    res.json(allUsers)
+})
+
+// show
+router.get('/:id', async (req, res)=> {
+    const foundUser = await db.User.findById(req.params.id)
+    const userChat = await db.Chat.find({ user: foundUser._id })
+    res.json({
+        user: foundUser,
+        chat: userChat
+    })
+})
+
+
+//update
+router.put('/:id', isAuthenticated, async (req, res) => {
+    const updatedUser = await db.User.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    )
+
+    res.json(updatedUser)
+})
+
+//delete
+router.delete('/:id', isAuthenticated, async (req, res)=> {
+    await db.Chat.deleteMany({ user: req.params.id})
+    await db.User.findByIdAndDelete(req.params.id)
+    res.sendStatus(200)
+})
+
+module.exports = router
